67e3bf2b58cc94dd463e56bfa789439e4b99b901
Danny Salman <Danny.Salman@ryerson.ca>
2020-06-17 16:08:32 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -23,7 +23,7 @@ sleep 240
 
 kubectl get deployments -n istio-system
 
-kubectl label namespace default istio-injection-ol=enabled
+kubectl label namespace default istio-injection=enabled
 
 cd ..
 
