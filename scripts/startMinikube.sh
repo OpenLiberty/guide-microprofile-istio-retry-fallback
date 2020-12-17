a38e6cdde7e93448b7ead365e52f0c93127cccb3
Austin Seto <austin.seto@ibm.com>
2020-12-17 11:55:12 -0500

    Revert curl change

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -6,7 +6,6 @@ chmod +x kubectl
 sudo ln -s -f $(pwd)/kubectl /usr/local/bin/kubectl
 wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
 chmod +x minikube
-CHANGE_MINIKUBE_NONE_USER=true
 
 sudo apt-get update -y
 sudo apt-get install -y conntrack
