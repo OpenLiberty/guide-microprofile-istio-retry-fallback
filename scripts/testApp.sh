0f874e1494aa5b117eb560a615c2aecb7bb016c0
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2021-01-08 13:13:28 -0500

    Add daily build to GH actions (#69)
    * daily build
    
    * executable
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Move minikube installation
    
    * Update testApp.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Move scripts around, check build level on docker image
    
    * Move istio installation
    
    * Update with new method of testing daily docker image
    
    * Change curl command in test to match the one in the guide
    
    * Revert curl change
    
    * Minor updates to set up scripts
    
    * Update istio install commands
    
    * Minor modification to setup and teardown
    
    * Update setup commands
    
    * Execute everything in same shell
    
    * Run setup/teardown in testApp.sh
    
    * Remove verbose on tar command
    
    * Check for port
    
    * Skip confirmation
    
    * Update test.yml
    
    Co-authored-by: Austin Seto <austin.seto@ibm.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

---
+++ scripts/testApp.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -euxo pipefail
+
+##############################################################################
+##
+##  GH actions CI test script
+##
+##############################################################################
+
+# Set up
+. ../scripts/startMinikube.sh
+. ../scripts/installIstio.sh
+
+# Test app
+
+kubectl get nodes
+
+mvn -q package
+
+docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+
+docker build -t system:1.0-SNAPSHOT system/.
+docker build -t inventory:1.0-SNAPSHOT inventory/.
+
+sleep 10
+
+kubectl apply -f services.yaml
+kubectl apply -f traffic.yaml
+
+sleep 180
+
+kubectl get pods
+
+kubectl get deployments
+
+kubectl get all -n istio-system
+
+SYSTEM=$(kubectl get pods | grep system | sed 's/ .*//')
+
+kubectl exec -it $SYSTEM -- /opt/ol/wlp/bin/server pause
+
+sleep 60
+
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+
+echo $(minikube ip):$INGRESS_PORT
+
+curl -H Host:inventory.example.com http://$(minikube ip):$INGRESS_PORT/inventory/systems/system-service -I
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+sleep 30
+
+COUNT=$(kubectl logs $SYSTEM -c istio-proxy | grep -c system-service:9080)
+
+echo COUNT=$COUNT
+
+kubectl exec $SYSTEM -- cat /logs/messages.log | grep product
+kubectl exec $SYSTEM -- cat /logs/messages.log | grep java
+
+if [ $COUNT -lt 3 ]; then
+    exit 1
+fi
+
+# Teardown
+
+. ../scripts/stopMinikube.sh
