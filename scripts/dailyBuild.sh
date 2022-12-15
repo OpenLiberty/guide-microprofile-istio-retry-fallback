610cc0ee06852be76d5329d6520595201b997325
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-15 11:49:41 -0800

    fix daily automation test failed (#102)
    * Update test.yml
    
    * Update dailyBuild.sh
    
    * Update testApp.sh
    
    * Update testApp.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -17,6 +17,6 @@ cat inventory/pom.xml system/pom.xml
 sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" inventory/Dockerfile system/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
 
-docker pull "$DOCKER_USERNAME""/olguides:""$BUILD"
+docker pull -q "$DOCKER_USERNAME""/olguides:""$BUILD"
 
-sudo ../scripts/testApp.sh
+sudo -u runner ../scripts/testApp.sh
